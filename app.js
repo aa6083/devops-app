51332fb9972c84f567b483052ff2f9c8869cd7e8
Affan Al Rayyan <151824768+aa6083@users.noreply.github.com>
2026-05-01 14:49:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ http.createServer((req, res) => {
     <body>
         <div class="card">
             <h1> ${version} Environment</h1>
-            <p>CI/CD Pipeline is working!</p>
+            <p>HELLLLLLLLLLLLLLLLLLLLLLLO</p>
 
             <button onclick="window.location.href='${current.switchUrl}'">
                 ${current.button}
